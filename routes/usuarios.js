37d135fcf26420cedb6da24d77d3cbbfb52b9215
pipe473 <bedoyafelipe743@gmail.com>
2020-06-12 21:54:52 +0200

    Creando un controller para los usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,13 @@
 // Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 // Crea un usuario
 // api/usuarios
 router.post('/', () => {
-
+    console.log('Creando un usuario...');
+    
 });
 
 module.exports = router;
