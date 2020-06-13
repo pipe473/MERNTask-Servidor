63b4931f4104e3c1452000d6871b6640e827c61f
pipe473 <bedoyafelipe743@gmail.com>
2020-06-13 21:59:37 +0200

    Eliminando un proyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -77,10 +77,38 @@ exports.actualizarProyecto = async (req, res) => {
         proyecto = await Proyecto.findOneAndUpdate({ _id: req.params.id }, { $set : nuevoProyecto}, {new: true});
 
         res.json({ proyecto });
-        
+
         
     } catch (error) {
         console.log(error);
         res.status(500).send('Error en el servidor');        
     }   
+}
+
+// Elimina un proyecto por su id
+exports.eliminarProyecto = async (req, res) => {
+   try {
+            // Revisar el ID
+        let proyecto = await Proyecto.findById(req.params.id);
+            
+
+        // Si el proyecto existe o no
+        if(!proyecto) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'})
+        }
+
+        // Verificar el creador del proyecto
+        if(proyecto.creador.toString() !== req.usuario.id ) {
+            return res.status(401).json({msg: 'No Autorizado'});
+        }
+
+        // Eliminar el proyecto
+        await Proyecto.findOneAndRemove({ _id: req.params.id });
+        res.json({ msg: 'Proyecto eliminado' });
+
+   } catch (error) {
+       console.log(error);
+       res.status(500).send({ msg: 'Error en el servidor' })
+       
+   }
 }
