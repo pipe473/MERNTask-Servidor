0d16e5a65c1555120347a6a885097746acf2de72
pipe473 <bedoyafelipe743@gmail.com>
2020-06-13 20:38:00 +0200

    Añadiendo nuevos proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,3 +1,15 @@
+const Proyecto = require('../models/Proyecto');
+
+
 exports.crearProyecto = async (req, res) => {
-    
+    try {
+        // Crear un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+        proyecto.save();
+        res.json(proyecto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ha habido un error');
+    }
 }
