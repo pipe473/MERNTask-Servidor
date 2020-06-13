93ff3ba685b732f7aceceb7f4912518f8bdb5202
pipe473 <bedoyafelipe743@gmail.com>
2020-06-13 20:17:05 +0200

    Revisando si el usuario existe, si el password es correcto o incorrecto

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,5 +10,42 @@ exports.autenticarUsuario = async (req, res) => {
         return res.status(400).json({errores: errores.array() })
     }
 
-    
+    // Extraer el email y el password 
+    const { email, password } = req.body;
+
+    try {
+        // Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+        if(!usuario) {
+            return res.status(400).json({msg: 'El usuario no existe'})
+        }
+
+        // Revisar el password 
+        const passCorrecto = await bcryptjs.compare(password, usuario.password);
+        if(!passCorrecto) {
+            return res.status(400).json({msg: 'Password incorrecto' })
+        }
+
+        // Si todo es correcto crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora de expiración del token
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación// Mensaje de confirmación
+            res.json({ token });
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
